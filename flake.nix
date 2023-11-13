5c30b3f3f3b04f84e600b864f4118e047bc41800
Johan Planchon <dev@planchon.dev>
2023-11-13 11:01:44 +0100

    feat: added naoqi devshell

--- flake.nix
+++ flake.nix
@@ -13,7 +13,7 @@
 
       naoqiOsVersion = "2.5.10";
       naoqiVersion = "2.5.7.1";
-      pythonNaoqi = pkgs.python2Packages.buildPythonPackage {
+      naoqi-py = pkgs.python2Packages.buildPythonPackage {
         pname = "naoqi";
         format = "other";
         version = naoqiOsVersion;
@@ -23,15 +23,28 @@
           sha256 = "sha256-0gYK1p+HSB8N2oLt5scMO2WvpvG/BuLBB8Ljc9JtksI=";
         };
 
+        nativeBuildInputs = [ pkgs.autoPatchelfHook ];
+        buildInputs = [ stdenv.cc.cc.lib pkgs.zlib pkgs.bzip2 ];
         installPhase = ''
-        mkdir -p $out
-        mv {bin,lib,share} $out
+          runHook preInstall
+          mkdir -p $out
+          mv {bin,lib,share} $out
+          chmod +x $out/lib/*.so
+          runHook postInstall
         '';
       };
-    in rec {
-      packages = {
-        naoqi = pythonNaoqi;
-        default = packages.naoqi;
+    in {
+      packages = rec {
+        naoqi = naoqi-py;
+        default = naoqi;
+      };
+      devShells = {
+        default = pkgs.mkShell {
+          buildInputs = with pkgs; [ python2 naoqi-py ];
+          shellHook = ''
+            export PYTHONPATH="${naoqi-py}/lib/python2.7/site-packages:$PYTHONPATH"
+          '';
+        };
       };
     });
 }
